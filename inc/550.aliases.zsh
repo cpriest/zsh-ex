d52844fc487d2725b7174efd476ab20556cc2f76
Clint Priest <cpriest@resultsgeneration.com>
2019-05-20 15:07:51 -0500

    Moved php* and zphp* aliases from pkzsh

--- inc/550.aliases.zsh
+++ inc/550.aliases.zsh
@@ -1,13 +1,18 @@
-#!/bin/zsh
+#!/usr/bin/zsh
 
 # PHP Related Aliases
-alias phps="php -ddisplay_startup_errors=1 -ddisplay_errors=1"
-alias phpi='php -v; php -ddisplay_startup_errors=1 -ddisplay_errors=1 -a';
-alias zphpi='zts-php -v; zts-php -ddisplay_startup_errors=1 -ddisplay_errors=1 -a';
-alias phpl='php -ddisplay_startup_errors=1 -ddisplay_errors=1 -l'
-alias zphpl='zts-php -ddisplay_startup_errors=1 -ddisplay_errors=1 -l'
-alias phpd='php -ddisplay_startup_errors=1 -ddisplay_errors=1 -dextension=dbg-php.so'
-alias zphpd='zts-php -ddisplay_startup_errors=1 -ddisplay_errors=1 -dextension=dbg-php.so'
-alias phpx='php -ddisplay_startup_errors=1 -ddisplay_errors=1 -dzend_extension=xdebug.so'
-alias zphpx='zts-php -ddisplay_startup_errors=1 -ddisplay_errors=1 -dzend_extension=xdebug.so'
-alias phplint='find . -name "*.php" -print0 | xargs -0 -n1 -P8 php -l'
+alias  php='php -ddisplay_startup_errors=1 -ddisplay_errors=1';
+alias zphp='zts-php -ddisplay_startup_errors=1 -ddisplay_errors=1';
+
+alias  phpd='php -dextension=dbg-php.so';
+alias zphpd='zts-php -dextension=dbg-php.so';
+alias  phpi='\php -v;     php -a';
+alias zphpi='\zts-php -v; zts-php -a';
+alias  phpl='php -l';
+alias zphpl='zts-php -l';
+alias  phpv='php -v';
+alias zphpv='zts-php -v';
+alias  phpx='php -dzend_extension=xdebug.so';
+alias zphpx='zts-php -dzend_extension=xdebug.so';
+
+alias phplint='find . -name "*.php" -print0 | xargs -0 -n1 -P8 php -l';
